a09ad7d8c8c8b53e1d1d4b5a4e33fcffa014ff42
Michael Samara <michaelsam08@gmail.com>
2017-10-12 21:16:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ function whisper(string) {
   return string.toLowerCase()
 }
 function logShout(string) {console.log(string.toUpperCase())}
-function logWhisper(string) {console.log(strin.toLowerCase())}
+function logWhisper(string) {console.log(string.toLowerCase())}
