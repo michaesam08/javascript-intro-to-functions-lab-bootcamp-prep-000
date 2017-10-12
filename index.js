7e81ca5d0bd63edd54778dbdb2839c5f3a09a4f6
Michael Samara <michaelsam08@gmail.com>
2017-10-12 21:41:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ function logWhisper(string) {console.log(string.toLowerCase())}
 
 function sayHiToGrandma(string){ console.log ("Hi Grandma".toLowerCase())
 return ("I can't hear you!"); }
-function sayHiToGrandma(string) {console.log("Hi Grandma".toUpperCase()) 
+function sayHiToGrandma(string) {console.log("Hi Grandma".toUpperCase())
 return ("YES INDEED");}
